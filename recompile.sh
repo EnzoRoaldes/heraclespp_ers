5caf845e5f7167342ebdc7743ac4967e916b5728
ROALDES Enzo <er283963@mdlsmc10.extra.cea.fr>
2025-06-30 10:41:47 +0200

    3 iter | compile on nodes (#ERS_10)

---
+++ recompile.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+## PDX-FileCopyrightText: 2025 The HERACLES++ development team, see COPYRIGHT.md file
+##
+## SPDX-License-Identifier: MIT
+
+rm build_heraclespp_*
+
+#SBATCH --job-name=build_heraclespp
+#SBATCH --output=build_heraclespp_%j.out
+#SBATCH --error=build_heraclespp_%j.err
+#SBATCH --time=00:30:00
+#SBATCH -C h100
+## #SBATCH --qos=qos_gpu_h100-dev
+
+## GPU allocation
+#SBATCH --gres=gpu:1
+#SBATCH --ntasks=1
+
+# Effacer les dossiers
+rm -rf build_H100/Hydro build_H100/nova++
+
+set -e
+
+## To clean and load modules defined at the compile and link phases
+module purge
+module load arch/h100
+module load cmake/3.31.4
+module load gcc/11.4.1
+module load cuda/12.8.0
+module load openmpi/4.1.5-cuda
+module load hdf5/1.12.0-mpi-cuda
+
+
+. vendor/install_pdi/share/pdi/env.sh
+
+export KOKKOS_TOOLS_LIBS=/linkhome/rech/genmdl01/ult48qa/kokkos-tools/profiling/nvtx-connector/kp_nvtx_connector.so
+
+# echo of commands
+set -x
+
+# Compile
+cmake --build build_H100 -j 16
+
